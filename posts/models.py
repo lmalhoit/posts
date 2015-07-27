bbab5ad5fb85f059eed2002dabafdd132506bcc5
lmalhoit <laurenmalhoit@gmail.com>
2015-07-27 12:10:25 -0700

    July 27

--- posts/models.py
+++ posts/models.py
@@ -2,3 +2,41 @@ from sqlalchemy import Column, Integer, String, Sequence
 
 from database import Base
 
+import datetime
+
+from sqlalchemy import Column, Integer, String, Text, DateTime
+
+from .database import Base, engine
+
+from flask.ext.login import UserMixin
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(1024))
+    content = Column(Text)
+    datetime = Column(DateTime, default=datetime.datetime.now)
+    author_id = Column(Integer, ForeignKey('users.id'))
+
+    def as_dictionary(self):
+        post = {
+            "id": self.id,
+            "title": self.title,
+            "body": self.content
+        }
+        return post
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))
+    posts = relationship("Post", backref="author")
+
+Base.metadata.create_all(engine)
