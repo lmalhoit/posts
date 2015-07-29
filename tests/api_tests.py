5a01714e74707e5f47e6f4555aab1e4f8d468958
lmalhoit <laurenmalhoit@gmail.com>
2015-07-29 16:19:23 -0700

    Filter by body content

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -101,8 +101,59 @@ class TestAPI(unittest.TestCase):
         response = self.client.delete("/post/postB.id")
 
         self.assertEqual(response.status_code, 404)
-        
 
+    def test_get_posts_with_title(self):
+        """ Filtering posts by title """
+        postA = models.Post(title="Post with bells", body="Just a test")
+        postB = models.Post(title="Post with whistles", body="Still a test")
+        postC = models.Post(title="Post with bells and whistles",
+                            body="Another test")
+
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get("/api/posts?title_like=whistles",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data)
+        self.assertEqual(len(posts), 2)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Post with whistles")
+        self.assertEqual(post["body"], "Still a test")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Post with bells and whistles")
+        self.assertEqual(post["body"], "Another test")
+
+    def test_get_posts_with_body(self):
+        """Filtering posts by body content"""
+        postA = models.Post(title="Post wtih bells", body="This is real")
+        postB = models.Post(title="Post with whistles", body="This is a test")
+        postC = models.Post(title="Post with bells and whistles", body="This is also a test")
+
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get("/api/posts?body_like=test", headers=[("Accept", "application/json")])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data)
+        self.assertEqual(len(posts), 2)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Post with whistles")
+        self.assertEqual(post["body"], "This is a test")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Post with bells and whistles")
+        self.assertEqual(post["body"], "This is also a test")
 
     def tearDown(self):
         """ Test teardown """
